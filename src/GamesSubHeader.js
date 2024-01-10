d31a2646a32e35da9b01e747cbf9c4356de65443
AlexandreMachadoGomes <alex.machado.cg@gmail.com>
2024-01-10 13:33:36 -0300

    Subheader animations

--- src/GamesSubHeader.js
+++ src/GamesSubHeader.js
@@ -1,20 +1,30 @@
 import { Link, useLocation } from 'react-router-dom';
+import React, { useState } from 'react';
 
 const GamesSubHeader = () => {
     const location = useLocation();
 
+    const [clicked, setClicked] = useState(false);
+
+    const handleClick = () => {
+        setClicked(true);
+        setTimeout(() => setClicked(false), 200); // Reset after animation
+    };
+
+
+
     if (location.pathname.startsWith('/my-games')) {
         return (
             <div className="games-subheader">
-                <Link to="/my-games/game1" className="game-link">
+                <Link to="/my-games/game1" className={`game-link ${clicked ? 'game-link-clicked' : ''}`} onClick={handleClick}>
                     <img src="/Envrion.jpg" alt="Envrion" className="game-thumbnail" />
                     <span>Envrion</span>
                 </Link>
-                <Link to="/my-games/game2" className="game-link">
+                <Link to="/my-games/game1" className={`game-link ${clicked ? 'game-link-clicked' : ''}`} onClick={handleClick}>
                     <img src="/Envrion.jpg" alt="Envrion" className="game-thumbnail" />
                     <span>Envrion</span>
                 </Link>
-                <Link to="/my-games/game3" className="game-link">
+                <Link to="/my-games/game1" className={`game-link ${clicked ? 'game-link-clicked' : ''}`} onClick={handleClick}>
                     <img src="/Envrion.jpg" alt="Envrion" className="game-thumbnail" />
                     <span>Envrion</span>
                 </Link>
